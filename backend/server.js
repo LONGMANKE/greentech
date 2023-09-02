7bcb726ba04516aec550eb4b5fcec5379f5964c1
Mburu Njoroge <90961555+LONGMANKE@users.noreply.github.com>
2023-09-02 23:30:11 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ process.on("uncaughtException", (err) => {
 
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "frontend/build")));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     });
   }
  
