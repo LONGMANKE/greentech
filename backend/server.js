fff1ca7b4eab3e45f8da665b25e8262e6d6530ee
Mburu Njoroge <90961555+LONGMANKE@users.noreply.github.com>
2023-09-02 23:32:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,15 @@ process.on("uncaughtException", (err) => {
 
 //config
 
-// dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "backend/config/config.env" });
 
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "frontend/build")));
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  //   });
+  // }
  
 //connecting with database 
 connectDatabase();
